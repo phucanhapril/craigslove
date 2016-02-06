5ac721d3a88409283d7f84b9a6dfba1dac31167e
fadyazmy <fadifessa@gmail.com>
2016-02-06 17:45:02 -0500

    python craigslist's scrapper

---
+++ craig.py
@@ -0,0 +1,69 @@
+from bs4 import BeautifulSoup
+from urllib2 import urlopen
+from datetime import datetime
+import csv
+import sys
+import os
+import smtplib
+import config
+
+# Craigslist search URL
+BASE_URL = ('http://chicago.craigslist.org/search/'
+            '?sort=rel&areaID=11&subAreaID=&query={0}&catAbb=sss')
+
+def parse_results(search_term):
+    results = []
+    search_term = search_term.strip().replace(' ', '+')
+    search_url = BASE_URL.format(search_term)
+    soup = BeautifulSoup(urlopen(search_url).read())
+    rows = soup.find('div', 'content').find_all('p', 'row')
+    for row in rows:
+        url = 'http://chicago.craigslist.org' + row.a['href']
+        # price = row.find('span', class_='price').get_text()
+        create_date = row.find('time').get('datetime')
+        title = row.find_all('a')[1].get_text()
+        results.append({'url': url, 'create_date': create_date, 'title': title})
+    return results
+
+
+def write_results(results):
+    """Writes list of dictionaries to file."""
+    fields = results[0].keys()
+    with open('results.csv', 'w') as f:
+        dw = csv.DictWriter(f, fieldnames=fields, delimiter='|')
+        dw.writer.writerow(dw.fieldnames)
+        dw.writerows(results)
+
+
+
+def get_current_time():
+    return datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
+
+if __name__ == '__main__':
+    try:
+        TERM = sys.argv[1]
+        PHONE_NUMBER = sys.argv[2].strip().replace('-', '')
+    except:
+        print "You need to include a search term and a 10-digit phone number!\n"
+        sys.exit(1)
+
+    if len(PHONE_NUMBER) != 10:
+        print "Phone numbers must be 10 digits!\n"
+        sys.exit(1)
+
+    
+    results = parse_results(TERM)
+    ## This will save the 
+    ##write_results(results) 
+    
+    ##print results
+    print results
+
+    ##print first results's url
+    print results[1]['url']
+
+
+
+
+## Now we access each link in the result and
+
