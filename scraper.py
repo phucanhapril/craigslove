a54e00c24348f439c4a3e00e03b5a12ecf64df4a
Miriam Ashton <miriam_ashton@brown.edu>
2016-02-07 02:23:34 -0500

    cleanup

--- scraper.py
+++ scraper.py
@@ -11,13 +11,6 @@ import config
 BASE_URL = ('http://providence.craigslist.org/search/'
             '?sort=rel&areaID=11&subAreaID=&query={0}&catAbb=sss')
 
-
-
-TEST_URL = ('http://providence.craigslist.org/w4m/5434907516.html')
-
-CSV = []
-page = []
-
 COLUMN_MAP = {
     "url": 0,
     "title": 1, 
@@ -32,23 +25,12 @@ COLUMN_MAP = {
     "eye color": 10,
     "religion": 11 }
 
+AD_TYPE = [ "stp", "w4w", "w4m", "m4w", "m4m", "msr", "cas", "mis", "rnr"]
 
-PROV_URL = ("http://providence.craigslist.org/search/stp")
-PROV_URL1 = ("http://providence.craigslist.org/search/w4w")
-PROV_URL2 = ("http://providence.craigslist.org/search/w4m")
-PROV_URL3 = ("http://providence.craigslist.org/search/m4w")
-PROV_URL4 = ("http://providence.craigslist.org/search/m4m")
-PROV_URL5 = ("http://providence.craigslist.org/search/msr")
-PROV_URL6 = ("http://providence.craigslist.org/search/cas")
-PROV_URL7 = ("http://providence.craigslist.org/search/mis")
-PROV_URL8 = ("http://providence.craigslist.org/search/rnr")
-
-
-
-def parse_search_result_page(search_term, URL_LINKER):
+def parse_search_result_page(search_url):
     results = []
-    search_term = search_term.strip().replace(' ', '+')
-    search_url = URL_LINKER.format(search_term)
+    # search_term = search_term.strip().replace(' ', '+')
+    # search_url = URL_LINKER.format(search_term)
     soup = BeautifulSoup(urlopen(search_url).read())
     rows = soup.find('div', 'content').find_all('p', 'row')
     for row in rows:
@@ -56,7 +38,6 @@ def parse_search_result_page(search_term, URL_LINKER):
             url = "http:" + row.a['href']
         else:     
             url = 'http://providence.craigslist.org' + row.a['href']
-        # price = row.find('span', class_='price').get_text()
         create_date = row.find('time').get('datetime')
         title = row.find_all('a')[1].get_text()
         results.append({'url': url, 'create_date': create_date, 'title': title})
@@ -86,8 +67,7 @@ def parse_page_result(URL_LINK):
             value = span.b.extract().get_text()
             key = span.get_text()[:-2].strip() # strip colons and trailing whitespace
             post[key] = value
-    print post
-    write_results_page(post)
+    return post
 
 
 
@@ -102,25 +82,6 @@ def write_results_page(page):
         outputWriter.writerow(valueList)
     except UnicodeEncodeError:
         pass
-
-
-
-# def write_results(results):
-#     """Writes list of dictionaries to file."""
-#     fields = results[0].keys()
-#     with open('results.csv', 'w') as f:
-#         dw = csv.DictWriter(f, fieldnames=fields, delimiter=', ')
-#         dw.writer.writerow(dw.fieldnames)
-#         dw.writerows(results)
-
-
-
-
-
-
-def get_current_time():
-    return datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
-
     
 
 if __name__ == '__main__':
@@ -128,96 +89,22 @@ if __name__ == '__main__':
     outputWriter = csv.writer(outputFile)
     orderedColumns = sorted(COLUMN_MAP, key=lambda k: COLUMN_MAP[k])
     outputWriter.writerow(orderedColumns);
-    #outputWriter.writerow(["title: ",  "text: ", "age: ", "status : ", "body : ","zodiac : ", "body art : ", "diet : ", "facial hair : ", "drinks : ","height : ", "eye color : ", "religion : "])
-    
-    try:
-        TERM = sys.argv[1]
-    except:
-        print "You need to include a search term and a 10-digit phone number!\n"
-        sys.exit(1)
-
-
-    
-    results = parse_search_result_page(TERM, PROV_URL)
-    for i in results:
-         # print type(i['url'])
-        print i['url']
-        ##print "hello"
-        parse_page_result(i['url'])
         
 
-    """
-    results = parse_search_result_page(TERM, PROV_URL1)
-    for i in results:
-         # print type(i['url'])
-        print i['url']
-        ##print "hello"
-        parse_page_result(i['url'])
+    # TODO search all pages for each query
+    # TODO search different cities
 
-
-    results = parse_search_result_page(TERM, PROV_URL2)
-    for i in results:
-         # print type(i['url'])
-        print i['url']
-        ##print "hello"
-        parse_page_result(i['url'])
-
-
-
-    results = parse_search_result_page(TERM, PROV_URL3)
-    for i in results:
-         # print type(i['url'])
-        print i['url']
-        ##print "hello"
-        parse_page_result(i['url'])
-
-
-    results = parse_search_result_page(TERM, PROV_URL4)
-    for i in results:
-         # print type(i['url'])
-        print i['url']
-        ##print "hello"
-        parse_page_result(i['url'])
-
-    results = parse_search_result_page(TERM, PROV_URL5)
-    for i in results:
-         # print type(i['url'])
-        print i['url']
-        ##print "hello"
-        parse_page_result(i['url'])
-
-
-
-    results = parse_search_result_page(TERM, PROV_URL6)
-    for i in results:
-         # print type(i['url'])
-        print i['url']
-        ##print "hello"
-        parse_page_result(i['url'])
-
-
-    results = parse_search_result_page(TERM, PROV_URL7)
-    for i in results:
-         # print type(i['url'])
-        print i['url']
-        ##print "hello"
-        parse_page_result(i['url'])
-
-    results = parse_search_result_page(TERM, PROV_URL8)
-    for i in results:
-         # print type(i['url'])
-        print i['url']
-        ##print "hello"
-        parse_page_result(i['url'])
-
-
-    """
-
-    
+    # do a search for every ad type
+    for adType in AD_TYPE:
+        print "searching " + adType
+        searchUrl = "http://providence.craigslist.org/search/" + adType
+        searchResults = parse_search_result_page(searchUrl)
+        # iterate thru search results and scrap each page
+        for ad in searchResults:
+            print "scraping " + ad['url']
+            parsedPost = parse_page_result(ad['url'])
+            # write parsed post to csv
+            write_results_page(parsedPost)
+        
     outputFile.close()
-
-
-
-
-## Now we access each link in the result and
 
