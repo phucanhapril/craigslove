7d4ad7acde96552b90682c9c7b1ab29ecaf352d9
Miriam Ashton <miriam_ashton@brown.edu>
2016-02-16 19:37:04 -0500

    refactoring and adding all attribute columns

--- scraper.py
+++ scraper.py
@@ -6,46 +6,72 @@ import sys
 import os
 import smtplib
 import config
+import re
 
-# Craigslist search ,
-BASE_URL = ('http://providence.craigslist.org/search/'
-            '?sort=rel&areaID=11&subAreaID=&query={0}&catAbb=sss')
+# Flag for scraping only first page of search results for each query (to not overwhelm craigslist when testing)
+FIRST_PAGE_ONLY = True
 
-COLUMN_MAP = {
-    "url": 0,
-    "title": 1, 
-    "text": 2,
-    "age": 3,
-    "city": 4,
-    "status": 5,
-    "body": 6,
-    "zodiac": 7,
-    "diet": 8,
-    "facial hair": 9,
-    "drinks": 10,
-    "eye color": 11,
-    "religion": 12 }
+COLUMNS = [
+    # required
+    'url',
+    'city', # need?
+    'datetime',
+    'category', # ex CAS
+    'type', # ex M4W
+    'title',
+    'text',
+    # optional
+    'age',
+    'body',
+    'body art',
+    'diet',
+    'dislikes',
+    'drinks',
+    'drugs',
+    'education',
+    'ethnicity',
+    'eye color',
+    'facial hair',
+    'fears',
+    'hair',
+    'height',
+    'hiv/hsv/hpv',
+    'interests',
+    'kids, have',
+    'kids, want',
+    'likes',
+    'native language',
+    'occupation',
+    'personality',
+    'pets',
+    'politics',
+    'religion',
+    'resembles',
+    'smokes',
+    'status',
+    'weight',
+    'zodiac' ]
 
-AD_TYPE = [ "stp", "w4w", "w4m", "m4w","m4m", "msr", "cas", "mis", "rnr"]
+PERSONALS_SECTIONS = [ "stp", "w4w", "w4m", "m4w","m4m", "msr", "cas", "mis", "rnr" ]
 
+# SEARCH_TYPES = [ 'w4m', 'm4m', 'm4w', 'w4w', 't4m', 'm4t', 't4w', 'w4t', 't4t', 'mw4mw', 'mw4w', 'mw4m',
+# 'w4mw', 'm4mw', 'w4ww', 'm4mm', 'mm4m', 'ww4w', 'ww4m', 'mm4w', 'm4ww', 'w4mm', 't4mw', 'mw4t' ]
 
 CITY_LINKS = [
-    #'providence.craigslist.org',
-    #'newyork.craigslist.org',
-    'sfbay.craigslist.org',
-    'washingtondc.craigslist.org',
-    'dallas.craigslist.org'
+    # 'providence.craigslist.org'
+    # 'newyork.craigslist.org'
+    # 'sfbay.craigslist.org',
+    # 'washingtondc.craigslist.org',
+     'dallas.craigslist.org'
     ]
 
-"""
-return a list of URLs from a personals search result
-"""
+# get a list of URLs from a personals search result
 def parse_search_result_page(search_url, city_url_root):
     
     soup = BeautifulSoup(urlopen(search_url).read(), "html.parser")
     rows = soup.find('div', 'content').find_all('p', 'row')
 
-    results = []
+    url_list = []
     for row in rows:
         
         """
@@ -55,70 +81,84 @@ def parse_search_result_page(search_url, city_url_root):
         postinghref = row.a['href'] 
         if not postinghref.startswith("//") and not postinghref.startswith("http") :
             url = 'http://' + city_url_root + postinghref
-            results.append(url)
+            url_list.append(url)
 
-    return results
+    return url_list
 
 
 # parse text and relevant attributes from a personal ad
-def parse_page_result(page_url, city):
-    #print URL_LINK
-    result_array = []
-    # search_url = URL_LINK.format(URL_LINK)
-    # print URL_LINK + " VS " + search_url
+def parse_page_result(page_url):
     try:
        soup = BeautifulSoup(urlopen(page_url).read(), "html.parser")
     except Exception as e:
         print e
         return
 
-    
-    # post represents the single personal ad on this page
+    # the post object represents the single personal ad on this page
     post = {}
 
-    post["city"] = city
-    post["url"] = page_url
-
-    if soup.title is not None:
+    # wrap the whole thing in a try-except because at any point the post may be marked for removal
+    try:
+        # get title
         post["title"] = soup.title.get_text()
-    
-    postingbody = soup.find(id="postingbody")
-    if postingbody is not None:
-        post["text"] = postingbody.get_text()
 
-    # find all attributes (age, status, body, etc)
-    attr_array = soup.find_all("p", "attrgroup")
-    for p in attr_array:
-        for span in p.find_all("span"):
-            if span.b is None:  
-                continue           
-            value = span.b.extract().get_text()
-            key = span.get_text()[:-2].strip() # strip colons and trailing whitespace
-            post[key] = value
+        # get text body
+        post["text"] = soup.find(id="postingbody").get_text()
+
+        # get time
+        post["datetime"] = soup.find('time', class_='timeago')["datetime"]
+
+        # find all attributes (age, status, zodiac, etc)
+
+        attr_array = soup.find_all("p", "attrgroup")
+        for p in attr_array:
+            for span in p.find_all("span"):
+                if span.b is not None:  
+                    value = span.b.extract().get_text()
+                    key_label = span.get_text()
+                    key = key_label[:-2].strip().lower()
+                    if key not in COLUMNS:
+                        print "!! CUSTOM ATTRIBUTE: " + key_label + " at " + page_url
+                    else:
+                        post[key] = value
+    except Exception as e:
+        print 'error at ' + page_url
+        print e
+
     return post
 
 
-# save a personal ad to a csv (as a single row)
-def write_post(post):
-    valueList = [None] * len(COLUMN_MAP)
+# convert post object to an ordered list, which can be written to a csv as one row
+def convert_to_row(post):
+    values = [None] * len(COLUMNS)
     for key in post:
-        if key in COLUMN_MAP:
-            index = COLUMN_MAP[key]
-            valueList[index] = post[key].encode('utf8')   
-    try:
-        outputWriter.writerow(valueList)
-    except UnicodeEncodeError as e:
-        print e
-    
+        col = COLUMNS.index(key)
+        if col > -1 and post[key]: #ignore custom attributes and keys with null values
+            values[col] = post[key].encode('utf8')
+    return values
 
-if __name__ == '__main__':
-    outputFile = open('results.csv', 'w')
-    outputWriter = csv.writer(outputFile)
-    orderedColumns = sorted(COLUMN_MAP, key=lambda k: COLUMN_MAP[k])
-    outputWriter.writerow(orderedColumns);
+"""
+get a post's 'category' (i.e. casual encounter, strictly platonic)
+along with its type (i.e. m4m, w4mw)
+"""
+def get_category_and_type(section, title):
+    # if in m4m/m4w/w4w/w4m, rename as "msr" (misc romance), and use the section as the search type
+    if '4' in section:
+        return ('msr', section)
+    
+    # otherwise, find the search type from the title string
+    m = re.search(' - .*4*$', title)
+    s_type = None
+    if (m):
+        s_type = m.group(0)[3:]
+    return (section, s_type)
+
+def main():
 
     # load location file containing craigslist urls for each city
     #file = open('locations.txt', 'r')
+
+    rows_to_write = []
 
     for city in CITY_LINKS:
         city_url = city.strip('\n')
@@ -126,25 +166,50 @@ if __name__ == '__main__':
         print "In city " + city_url
 
         # do a search for every ad type
-        for adType in AD_TYPE:
-            print "searching " + adType
+        for section in PERSONALS_SECTIONS:
+            print "searching " + section
             count = 0
-            searchUrl = "http://" + city_url + "/search/" + adType + "/?s=" + str(count)
+            searchUrl = "http://" + city_url + "/search/" + section + "/?s=" + str(count)
 
             searchResults = parse_search_result_page(searchUrl, city_url)
             
             while len(searchResults) > 0:
                 print "searching count " + str(count)
-                # iterate thru search results and scrap each page
+                # iterate thru search results and scrape each page
                 for adUrl in searchResults:
-                    print "scraping " + adUrl
-                    parsedPost = parse_page_result(adUrl, city_name)
-                    # write parsed post to csv
-                    write_post(parsedPost)
+                    # print "scraping " + adUrl
+                    parsedPost = parse_page_result(adUrl)
+                    # post may be null if it has been flagged for removal
+                    if (parsedPost):
+                        # add aditional info to post
+                        parsedPost["url"] = adUrl
+                        parsedPost["city"] = city_name
+                        category,search_type = get_category_and_type(section, parsedPost["title"])
+                        parsedPost["category"] = category
+                        parsedPost["type"] = search_type
+
+                        rows_to_write.append(convert_to_row(parsedPost))
+
+                if FIRST_PAGE_ONLY:
+                    break
 
                 count += 100
-                searchUrl = "http://" + city_url + "/search/" + adType + "/?s=" + str(count)
+                searchUrl = "http://" + city_url + "/search/" + section + "/?s=" + str(count)
                 searchResults = parse_search_result_page(searchUrl, city_url)
-        
+    
+
+    outputFile = open('results.csv', 'w')
+    outputWriter = csv.writer(outputFile)
+
+    # write header
+    outputWriter.writerow(COLUMNS);
+
+    # write all rows
+    for row in rows_to_write:
+        outputWriter.writerow(row)
+
     outputFile.close()
+
+if __name__ == '__main__':
+    main()
 
