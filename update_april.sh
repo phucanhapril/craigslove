0f827fc8d55a782af29a5af8089858c83304e509
April Tran <april_tran@brown.edu>
2016-03-19 01:13:47 -0400

    Update OKCity, Chitown, Vegas, LA

--- update_april.sh
+++ update_april.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+git pull
 python craigslove.py oklahomacity.craigslist.org
 python craigslove.py chicago.craigslist.org
 python craigslove.py lasvegas.craigslist.org
 python craigslove.py losangeles.craigslist.org
+git commit -am 'Update OKCity, Chitown, Vegas, LA'
+git push
