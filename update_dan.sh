9b0ab9c2b9e153c127ec30caeef24522a4e34d01
danwang1995 <danwang1995@gmail.com>
2016-03-24 03:25:58 -0400

    Dallas, Denver, Miami, Providence

--- update_dan.sh
+++ update_dan.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 git pull
 python craigslove.py dallas.craigslist.org
+python remove_duplicates.py results/dallas
 python craigslove.py denver.craigslist.org
+python remove_duplicates.py results/denver
 python craigslove.py miami.craigslist.org
+python remove_duplicates.py results/miami
 python craigslove.py providence.craigslist.org
+python remove_duplicates.py results/providence
 git add .
 git commit . -m "Dallas, Denver, Miami, Providence"
 git pull
