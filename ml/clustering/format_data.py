e3396da5b228475ef642f55647c19b199fe7f5cf
Miriam Ashton <miriam_ashton@brown.edu>
2016-05-02 21:10:15 -0400

    clustering without data

--- ml/clustering/format_data.py
+++ ml/clustering/format_data.py
@@ -6,36 +6,105 @@ format data for clustering
 """
 
 results_dir = '../../results'
-output_file = 'formatted_posts.csv'
+
+MINIMUM_POSTS = 100 #minimum to save a category
+
+
+def write_rows(filename, rows):
+	if len(rows) < MINIMUM_POSTS:
+		return
+
+	print 'Saving ' + str(len(rows)) + ' rows to ' + filename
+	with open(filename, 'w') as out_file:
+		csv_writer = csv.writer(out_file)
+		for p in rows:
+			csv_writer.writerow(p)
+
+def by_city():
+	for city in os.listdir(results_dir):
+		d_path = os.path.join(results_dir, city)
+		if os.path.isdir(d_path):
+			city_posts = []
+			for f in os.listdir(d_path):
+				if f.endswith('.csv'): 
+					with open(os.path.join(d_path, f),'r') as in_file:
+						next(in_file) # skip column labels
+						csv_reader = csv.reader(in_file)
+						for line in csv_reader:
+							city = line[1]
+							title = line[7]
+							text = line[8] + title + title # DOUBLE WEIGHT WORDS IN TITLE
+							category = line[5]
+							posttype = line[6]
+							city_posts.append([text, city, category, posttype]) #text
+
+			write_rows('data/city/' + city + '.csv', city_posts)
+
+def by_field(dirname, column_number):
+	rows_by_field = {}
+	for city in os.listdir(results_dir):
+		d_path = os.path.join(results_dir, city)
+		if os.path.isdir(d_path):
+			posts = []
+			for f in os.listdir(d_path):
+				if f.endswith('.csv'): 
+					with open(os.path.join(d_path, f),'r') as in_file:
+						next(in_file) # skip column labels
+						csv_reader = csv.reader(in_file)
+						for line in csv_reader:
+							city = line[1]
+							title = line[7]
+							text = line[8] + title + title # DOUBLE WEIGHT WORDS IN TITLE
+							category = line[5]
+							posttype = line[6]
+							# split based on value of the column we're concerned with
+							value = line[column_number]
+							if not value:
+								value = 'none'
+							if value not in rows_by_field:
+								rows_by_field[value] = []
+							rows_by_field[value].append([text, city, category, posttype]) #text
+
+	for value in rows_by_field:
+		write_rows('data/' + dirname + '/' + value + '.csv', rows_by_field[value])
+
+"""
+split up all posts into those posted by males (for anyone) and those posted by females (for anyone)
+* this excludes posts coming from two people (ie mw4m, mm4m, etc)
+* this includes both platonic and romantic postss
+
+"""
+def by_gender():
+	male = []
+	female = []
+	for city in os.listdir(results_dir):
+		d_path = os.path.join(results_dir, city)
+		if os.path.isdir(d_path):
+			posts = []
+			for f in os.listdir(d_path):
+				if f.endswith('.csv'): 
+					with open(os.path.join(d_path, f),'r') as in_file:
+						next(in_file) # skip column labels
+						csv_reader = csv.reader(in_file)
+						for line in csv_reader:
+							city = line[1]
+							title = line[7]
+							text = line[8] + title + title # DOUBLE WEIGHT WORDS IN TITLE
+							category = line[5]
+							posttype = line[6]
+							# split on male and female posters
+							if posttype.startswith('m4'):
+								male.append([text, city, category, posttype])
+							elif posttype.startswith('w4'):
+								female.append([text, city, category, posttype])
+
+	write_rows('data/gender/male.csv', male)
+	write_rows('data/gender/female.csv', female)
 
 def main():
-	# process each csv in each subdirectory
-	posts_to_write = []
-
-	for d in os.listdir(results_dir):
-		d_path = os.path.join(results_dir, d)
-		if os.path.isdir(d_path):
-			print d_path
-			if 'sfbay' in d_path: # ONLY PROVIDENCE REST IS TOO BIG
-				for f in os.listdir(d_path):
-					if f.endswith('.csv'): 
-						csv_file = os.path.join(d_path, f)
-						with open(csv_file,'r') as in_file:
-							next(in_file)
-							csv_reader = csv.reader(in_file)
-							for line in csv_reader:
-								# extract only the content we want
-								# content = []
-								# content.append(line[1]) #city
-								# content.append(line[8]) #text
-								posts_to_write.append([line[8]]) #text
-
-	print ' saving ' + str(len(posts_to_write)) + ' posts'
-	with open(output_file, 'w') as out_file:
-		csv_writer = csv.writer(out_file)
-		for p in posts_to_write:
-			csv_writer.writerow(p)
-	return posts_to_write
+	#by_city()
+	#by_field('type', 6)
+	by_gender()
 
 if __name__ == '__main__':
 	main()
