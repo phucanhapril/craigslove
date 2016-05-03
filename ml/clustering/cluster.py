0cb5149d39bd6a6604f0ca0c5eaa89fa294468a6
Miriam Ashton <miriam_ashton@brown.edu>
2016-05-03 16:34:51 -0400

    update clustering so clusters are saved to json files

--- ml/clustering/cluster.py
+++ ml/clustering/cluster.py
@@ -1,37 +1,38 @@
 
-import numpy as np
-import re
 import os
+import sys
 import csv
+import json
+import re
 import nltk
-import pandas as pd
 import time
+import argparse
+import pandas as pd
+import numpy as np
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.cluster import KMeans
 from sklearn.externals import joblib
 from nltk.stem.snowball import SnowballStemmer
 
-import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
 """
 first create data using format_data.py
+
+cluster.py will output clusters in json format to the cluster_data/ directory
 """
 
 
 # reference: http://brandonrose.org/clustering
-
-
-punc = re.compile(ur"[^\w\d'\s]+")
-data_file = 'formatted_posts_all.csv' #'small_sample.csv'
 
 # which words are not interesting?
 stopwords = ['guy', 'guys', 'girl', 'girls', 'woman', 'women', 'man', 'men', 'pics', 'pic', 'send', 'reply', 'good', 'age']
 
 stemmer = SnowballStemmer("english")
 
-def cluster(data_file):
+def cluster(data_file, num_clusters):
 	# read posts from file
 	texts = []
 	cities = []
@@ -65,8 +66,6 @@ def cluster(data_file):
 	dist = 1 - cosine_similarity(tfidf_matrix) #TODO look at this
 	print dist
 
-	num_clusters = 3
-
 	t3 = time.clock()
 	km = KMeans(n_clusters=num_clusters)
 	km.fit(tfidf_matrix)
@@ -74,7 +73,7 @@ def cluster(data_file):
 	print("KMeans fit: {} seconds.\n".format(t4 - t3))
 
 	joblib.dump(km, 'doc_cluster.pkl')
-	
+
 	# save the km clusters so we dont recompute every time
 	"""
 	km = joblib.load('doc_cluster.pkl')
@@ -85,38 +84,38 @@ def cluster(data_file):
 	posts = {'category': categories, 'type': types, 'city': cities, 'text': texts, 'cluster': clusters }
 
 	frame = pd.DataFrame(posts, index = [clusters] , columns = ['cluster', 'category', 'type', 'city', 'text'])
-	#print frame['cluster'].value_counts()
 
 	order_centroids = km.cluster_centers_.argsort()[:, ::-1]
+
+
+	# convert clusters to json objects
+
+	clusters_as_json = {}
+
+	NUM_TOP_TERMS = 20
+	NUM_TOP_FIELDS = 8
+
 	for i in range(num_clusters):
-		print ''
-		print ' CLUSTER ' + str(i+1)
+		cluster_json = {}
 
-		# TOP TERMS
-		print ''
-		print '~ terms ~'
-		for j in order_centroids[i, :10]:
-			print terms[j]
+		# top terms
+		cluster_json['terms'] = [ terms[j] for j in order_centroids[i, :NUM_TOP_TERMS]]
 		
-		# TOP TYPES
-		print ''
-		print '~ types ~'
+		# top types (ie m4m, m4w)
 		types = frame.ix[i]['type'].values.tolist()
-		print pd.value_counts(types)[:4]
+		cluster_json['types'] = [x for x in pd.value_counts(types)[:NUM_TOP_FIELDS].iteritems()]
 
-		# TOP CATEGORIES
-		print ''
-		print '~ categories ~'
-		categories = frame.ix[i]['category'].values.tolist()
-		print pd.value_counts(categories)[:4]
+		# top categories (msr or stp)
+		cluster_json['categories'] = []
+		cluster_json['categories'] = [x for x in pd.value_counts(categories)[:NUM_TOP_FIELDS].iteritems()]
 
-		# TOP CITIES
-		print ''
-		print '~ cities ~'
+		# top cities
 		cities = frame.ix[i]['city'].values.tolist()
-		print pd.value_counts(cities)[:4]
-		#TODO write results to file! so can visualize/compare
+		cluster_json['cities'] = [x for x in pd.value_counts(cities)[:NUM_TOP_FIELDS].iteritems()]
 
+		clusters_as_json[str(i+1)] = cluster_json
+
+	return clusters_as_json
 
 def tokenize(text):
 	# first tokenize by sentence, then by word
@@ -129,16 +128,40 @@ def tokenize(text):
 				filtered_tokens.append(stemmer.stem(token))
 	return filtered_tokens
 
+def get_cluster_filename_from_csv(csv_file_name):
+	return 'cluster_data/' + csv_file_name.split('/')[-1:][0][:-4] + '_clusters.json'
+
 def main():
-	if len(sys.argv) < 2:
-		print 'Usage: python cluster.py file.csv'
-		return
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-path', required=True, help='either a csv file OR a directory containing csv files')
+	parser.add_argument('-c', required=True, help='number of clusters')
+	opts = parser.parse_args()
+
+	num_clusters = int(opts.c)
 
 	# add english language stopwords from file
 	with open('stopwords.txt') as f:
 		stopwords.extend(f.read().splitlines())
 
-	cluster(sys.argv[1]) # TODO return results and write to file
+	# process entire directory of csv files
+	if os.path.isdir(opts.path):
+		for f in os.listdir(opts.path):
+			if f.endswith('.csv'): 
+				file_path = os.path.join(opts.path, f)
+				clusters_obj = cluster(file_path, num_clusters)
+				print 'Saving clusters from ' + file_path
+				with open(get_cluster_filename_from_csv(file_path), 'w') as outfile:
+					json.dump(clusters_obj, outfile)
+
+	# process only one csv file
+	elif os.path.isfile(opts.path) and opts.path.endswith('.csv'):
+		clusters_obj = cluster(opts.path, num_clusters)
+		with open(get_cluster_filename_from_csv(opts.path), 'w') as outfile:
+			json.dump(clusters_obj, outfile)
+
+
+	else:
+		print 'error in ' + opts.path
 
 if __name__ == '__main__':
 	main()
