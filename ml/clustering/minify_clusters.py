8b025706760a722ef251ce1928dd3beff54fa03d
Miriam Ashton <miriam_ashton@brown.edu>
2016-05-07 10:34:05 -0400

    all 2means city clusters grouped in a single file

---
+++ ml/clustering/minify_clusters.py
@@ -0,0 +1,43 @@
+import os
+import numpy as np
+import json
+from collections import Counter
+
+"""
+group all clutsres from each city into a single file and save it
+then we can go through manually and attach titles
+"""
+
+OUTPUT_FILE = 'cluster_data/all_city_clusters.json'
+INPUT_DIR = 'cluster_data/cities_2means'
+
+def main():
+	all_words = []
+	all_clusters = []
+
+	for f in os.listdir(INPUT_DIR):
+
+		if f.endswith('.json'): 
+			city_name = f.split('_')[0]
+			f_path = os.path.join(INPUT_DIR, f)
+
+			with open(f_path,'r') as json_file:
+				data = json.load(json_file)
+				# for every cluster in thie file
+				for c in data.keys():
+					all_words.extend(data[c]['terms'])
+					minified = {}
+					minified['city'] = city_name
+					minified['name'] = '' # TO FILL IN MANUALLY
+					minified['terms'] = data[c]['terms']
+					all_clusters.append(minified)
+
+	with open(OUTPUT_FILE, 'w') as f:
+		json.dump(all_clusters, f)
+
+
+	c = Counter(all_words)
+	print c
+
+if __name__ == '__main__':
+	main()
