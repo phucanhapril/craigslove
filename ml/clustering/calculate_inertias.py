fd08bd5db204fedbb470e6e395913f5686b9b92a
Miriam Ashton <miriam_ashton@brown.edu>
2016-05-13 22:50:21 -0400

    moving jaccard stuff into a dir

--- ml/clustering/calculate_inertias.py
+++ ml/clustering/calculate_inertias.py
@@ -3,7 +3,6 @@ import re
 import os
 import csv
 import nltk
-# import pandas as pd
 import time
 import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -16,10 +15,14 @@ from collections import defaultdict
 import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
-"""
-first create data using format_data.py
+
 """
 
+***** see cluster.py file for info about the actual clustering *****
+
+this is for calculating inertias in order to use the 'elbow method'
+
+"""
 
 # reference: http://brandonrose.org/clustering
 
@@ -27,8 +30,8 @@ first create data using format_data.py
 punc = re.compile(ur"[^\w\d'\s]+")
 data_file = 'formatted_posts_all.csv' #'small_sample.csv'
 
-# which words are not interesting?
-stopwords = ['guy', 'guys', 'girl', 'girls', 'woman', 'women', 'man', 'men', 'pics', 'pic', 'send', 'reply', 'good', 'age']
+# which words are not interesting
+stopwords = ['male', 'female', 'guy', 'guys', 'girl', 'girls', 'woman', 'women', 'man', 'men', 'year', 'age', 'good', 'send', 'seek','seeking', 'find', 'thing', 'go' ,'great', 'time', 'email', 'subject', 'reply']
 
 stemmer = SnowballStemmer("english")
 
@@ -61,11 +64,6 @@ def calculate_inertias(data_file, min_clusters, max_clusters):
 
     terms = tfidf_vectorizer.get_feature_names() #a list of the features used in the tf-idf matrix
     
-    print terms
-
-    dist = 1 - cosine_similarity(tfidf_matrix) #TODO look at this
-    print dist
-    
     x_axis = np.arange(min_clusters, max_clusters + 1, 1)
     y_axis = []
     inertias = defaultdict(float)
@@ -77,49 +75,6 @@ def calculate_inertias(data_file, min_clusters, max_clusters):
         t4 = time.clock()
         print("KMeans fit: {} seconds.\n".format(t4 - t3))
 
-        joblib.dump(km, 'doc_cluster.pkl')
-        
-        # save the km clusters so we dont recompute every time
-        """
-        km = joblib.load('doc_cluster.pkl')
-        """
-        clusters = km.labels_.tolist()
-
-        posts = {'category': categories, 'type': types, 'city': cities, 'text': texts, 'cluster': clusters }
-
-        # frame = pd.DataFrame(posts, index = [clusters] , columns = ['cluster', 'category', 'type', 'city', 'text'])
-        # print frame['cluster'].value_counts()
-
-        order_centroids = km.cluster_centers_.argsort()[:, ::-1]
-        for i in range(num_clusters):
-            print ''
-            print ' CLUSTER ' + str(i+1)
-
-            # TOP TERMS
-            print ''
-            print '~ terms ~'
-            for j in order_centroids[i, :10]:
-                print terms[j]
-            
-            # TOP TYPES
-            print ''
-            print '~ types ~'
-            # types = frame.ix[i]['type'].values.tolist()
-            # print pd.value_counts(types)[:4]
-
-            # TOP CATEGORIES
-            print ''
-            print '~ categories ~'
-            # categories = frame.ix[i]['category'].values.tolist()
-            # print pd.value_counts(categories)[:4]
-
-            # TOP CITIES
-            print ''
-            print '~ cities ~'
-            # cities = frame.ix[i]['city'].values.tolist()
-            # print pd.value_counts(cities)[:4]
-            #TODO write results to file! so can visualize/compare
-        
         inertias[num_clusters] = km.inertia_
         y_axis.append(km.inertia_)
     
@@ -134,9 +89,15 @@ def tokenize(text):
     filtered_tokens = []
     # filter out any tokens not containing letters (raw punctuation)
     for token in tokens:
-        if re.search('[a-zA-Z0-9]', token):
+        # the only numerical word that we want to keep is 420. The rest are ages, heights, weights, etc
+        if re.search('[a-zA-Z]', token) or token == '420':
             if token not in stopwords:
-                filtered_tokens.append(stemmer.stem(token))
+                stem = stemmer.stem(token)
+                if stem not in stopwords:
+                    if stem == 'pictur':
+                        stem = 'pic' # group these together because they both appear a lot and mean the same thing
+                    filtered_tokens.append(stem)
+
     return filtered_tokens
 
 def main():
@@ -145,10 +106,10 @@ def main():
         return
 
     # add english language stopwords from file
-    with open('stopwords.txt') as f:
+    with open('util/stopwords.txt') as f:
         stopwords.extend(f.read().splitlines())
 
-    calculate_inertias(sys.argv[1], 1, 5) # TODO return results and write to file
+    calculate_inertias(sys.argv[1], 1, 5)
 
 if __name__ == '__main__':
     main()
